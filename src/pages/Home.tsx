6622bb42e8cf773bb7298651b7b44d2756997d54
Dimas Febriyanto <dimassfeb@gmail.com>
2023-07-11 09:01:32 +0700

    updated home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <div className="h-screen flex flex-col gap-4 justify-center mx-56">
+    <div className="h-screen flex flex-col gap-4 justify-center mx-5 md:mx-12 lg:mx-56">
       <div className="text-6xl font-bold">Hi! I am Dimas Febriyanto ✌️</div>
       <div className="text-xl">
         I am a passionate Web Developer, specialised in back-end development
