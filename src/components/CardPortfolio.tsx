c03928e546e4345c1513e87686cb5cf9f3f813cd
Dimas Febriyanto <dimassfeb@gmail.com>
2023-12-23 12:01:43 +0700

    updated portfolio

--- src/components/CardPortfolio.tsx
+++ src/components/CardPortfolio.tsx
@@ -1,32 +1,17 @@
-import { Android, GitHub, Preview, Public, Shop } from "@mui/icons-material";
+import { GitHub, Preview, Shop } from "@mui/icons-material";
 
 function CardPortfolio(props: Portfolio) {
   const { image_path, playstore, title, platform, github, tech, demo_live } =
     props;
 
   return (
-    <div className="flex flex-col p-5 mx-2 md:mx-0 gap-2 rounded-md bg-white dark:bg-darkColor border-2 dark:border-[1px] duration-300 hover:scale-105 hover:drop-shadow-xl ">
+    <div className="flex flex-col p-5 md:mx-0 gap-2 rounded-md bg-white dark:bg-darkColor border-2 dark:border-[1px] duration-300">
       <img src={image_path} className="aspect-video" alt={image_path} />
       <hr />
-      <div className="h-full justify-between flex flex-col gap-4 ">
+      <div className="h-full  flex flex-col gap-4 ">
         <div className="flex flex-col gap-2">
           <div className="font-bold text-ellipsis">
             <span>{title}</span>
-            <span className="group/platform ml-1 inline-flex justify-center">
-              <span
-                className="hidden group-hover/platform:inline-block rounded group-hover/platform:absolute
-                                -translate-y-6 bg-black text-white text-xs p-1"
-              >
-                {platform.toLowerCase()}
-              </span>
-              <span>
-                {platform == "mobile" ? (
-                  <Android fontSize="small" className="text-green-700" />
-                ) : (
-                  <Public fontSize="small" className="text-blue-500" />
-                )}
-              </span>
-            </span>
           </div>
           <div className="flex">
             {tech.map((v, _) => {
@@ -54,33 +39,35 @@ function CardPortfolio(props: Portfolio) {
             })}
           </div>
         </div>
-        <div className="flex text-black dark:text-white">
-          {playstore ? (
-            <a href={playstore} target="_blank" className="mr-3">
-              <div className="flex items-center text-[1rem] gap-2 py-1 px-2 border border-black dark:border-white transition-colors duration-100 ease-in-out hover:scale-105 hover:bg-black hover:text-white">
-                <Shop /> Playstore
+        <div className="flex items-center justify-between mt-5">
+          <div className="flex text-black dark:text-white">
+            {playstore ? (
+              <a href={playstore} target="_blank" className="mr-3">
+                <div className="flex items-center text-[1rem] gap-2 py-1 px-2 border border-primary dark:border-white transition-colors duration-100 ease-in-out hover:scale-105 hover:bg-secondary hover:text-white">
+                  <Shop /> Playstore
+                </div>
+              </a>
+            ) : (
+              <div></div>
+            )}
+
+            <a href={github} target="_blank" className="group mr-3">
+              <div className="flex items-center text-[1rem] gap-2 py-1 px-2 border border-gray-300 dark:border-white transition-colors duration-100 ease-in-out hover:scale-105 hover:bg-secondary hover:text-white">
+                <GitHub />
+                Github
               </div>
             </a>
-          ) : (
-            <div></div>
-          )}
 
-          <a href={github} target="_blank" className="group mr-3">
-            <div className="flex items-center text-[1rem] gap-2 py-1 px-2 border border-black dark:border-white transition-colors duration-100 ease-in-out hover:scale-105 hover:bg-black hover:text-white">
-              <GitHub />
-              Github
-            </div>
-          </a>
-
-          {demo_live ? (
-            <a href={demo_live} target="_blank" className="mr-3">
-              <div className="flex items-center text-[1rem] gap-2 py-1 px-2 border border-black dark:border-white transition-colors duration-100 ease-in-out hover:scale-105 hover:bg-black hover:text-white">
-                <Preview /> Demo
-              </div>
-            </a>
-          ) : (
-            <div></div>
-          )}
+            {demo_live ? (
+              <a href={demo_live} target="_blank" className="mr-3">
+                <div className="flex items-center text-[1rem] gap-2 py-1 px-2 border border-gray-300 dark:border-white transition-colors duration-100 ease-in-out hover:scale-105 hover:bg-secondary hover:text-white">
+                  <Preview /> Demo
+                </div>
+              </a>
+            ) : (
+              <div></div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -3,6 +3,12 @@ import { collection, getDocs } from "@firebase/firestore";
 import { db } from "../db/Firebase.ts";
 import { useEffect, useState } from "react";
 import LoadingScreen from "../components/LoadingScreen.tsx";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay, Pagination } from "swiper/modules";
+import { ArrowRightAltOutlined } from "@mui/icons-material";
 
 const Portfolio = () => {
   const [portfolios, setPortfolios] = useState<Portfolio[]>([]);
@@ -41,28 +47,53 @@ const Portfolio = () => {
   }
 
   return (
-    <div className="dark:bg-darkColor dark:text-white h-auto xl:px-20">
-      <div className="group/portfolio m-5 w-min font-bold text-6xl">
+    <div className="mb-20 px-5 sm:px-20">
+      <div className="w-min font-bold text-6xl mb-10">
         <div className="text-secondary font-bold">
           <span className="text-black">#</span>Portfolio
           <span className="text-black">.</span>
         </div>
       </div>
-      <div className="p-5 gap-7 xl:gap-10 grid md:grid-cols-2 lg:grid-cols-3 overflow-y-auto  auto-rows-auto justify-center">
-        {portfolios.map(function (object, _) {
-          return (
-            <CardPortfolio
-              title={object.title}
-              platform={object.platform}
-              image_path={object.image_path}
-              playstore={object.playstore}
-              github={object.github}
-              tech={object.tech}
-              demo_live={object.demo_live}
-              key={object.title}
-            ></CardPortfolio>
-          );
-        })}
+      <div>
+        <Swiper
+          slidesPerView={"auto"}
+          spaceBetween={30}
+          modules={[Autoplay, Pagination]}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            768: {
+              slidesPerView: 3,
+            },
+            320: {
+              slidesPerView: 1,
+            },
+          }}
+          className="mySwiper"
+        >
+          {portfolios.map(function (object, _) {
+            return (
+              <SwiperSlide>
+                <CardPortfolio
+                  title={object.title}
+                  platform={object.platform}
+                  image_path={object.image_path}
+                  playstore={object.playstore}
+                  github={object.github}
+                  tech={object.tech}
+                  demo_live={object.demo_live}
+                  key={object.title}
+                ></CardPortfolio>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        <div className="mt-[0.7rem]">
+          <span className="font-bold text-secondary">Swipe to Right</span>
+          <ArrowRightAltOutlined />
+        </div>
       </div>
     </div>
   );
